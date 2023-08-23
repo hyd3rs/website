83d62290719605bcbef0a9efdf675c227344a0c3
Dani <git@danny.works>
2023-08-22 21:53:26 -0700

    add mindful warning for light mode

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,6 +2,7 @@ import { NavigationRoot } from '../components/Navigation';
 import './globals.css'
 import { Fredoka, Golos_Text, Inter, Outfit, Rubik } from 'next/font/google'
 import { ColourProvider } from './Providers';
+import { AiFillWarning } from 'react-icons/ai';
 
 const inter = Inter({ subsets: ['latin'], variable: "--var-inter", display: "swap" })
 const outfit = Outfit({ subsets: ['latin'], variable: "--var-outfit", display: "swap" });
@@ -16,11 +17,17 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" className="dark">
             <ColourProvider>
-                <body className={`${outfit.variable} ${inter.variable} ${fredoka.variable} font-sans px-8 sm:px-16 lg:px-48 xl:px-60 2xl:px-96 py-16`}>
-                        <NavigationRoot />
-                        <main className="flex flex-col min-h-screen py-8">
-                            {children}
-                        </main>
+                <body className={`${outfit.variable} ${inter.variable} ${fredoka.variable} font-sans`}>
+                        <div className="dark:hidden flex flex-row items-center gap-x-2 w-full p-2 bg-red-400 font-medium font-fredoka lg:text-2xl">
+                            <AiFillWarning className="text-6xl lg:text-3xl text-white" />
+                            <span className="text-white">Howdy there! Light mode is currently a huge mess and an eyesore. Do pardon the mess, you should probably go back to dark mode.</span>
+                        </div>
+                        <div className="px-8 sm:px-16 lg:px-48 xl:px-60 2xl:px-96 py-16">
+                            <NavigationRoot />
+                            <main className="flex flex-col min-h-screen py-8">
+                                {children}
+                            </main>
+                        </div>
                 </body>
             </ColourProvider>
         </html>
