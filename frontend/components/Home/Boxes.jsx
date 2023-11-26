5801b3a4faf2964a1332acf00f59e5ff972fa688
Dani <git@danny.works>
2023-11-26 14:48:34 -0800

    fixed ugly hobbies

--- frontend/components/Home/Boxes.jsx
+++ frontend/components/Home/Boxes.jsx
@@ -2,7 +2,7 @@ import { Tooltip } from "@mui/joy";
 import Link from "next/link";
 
 export const StandardBox = ({ title, children, right }) => (
-    <div className={`flex flex-col gap-y-2 px-6 py-8 dark:bg-[#321d4e] bg-[#c6abe9] rounded-xl h-full ${right ? "md:text-right md:items-end md:flex-col-reverse" : "text-left items-start"} justify-center`}>
+    <div className={`flex flex-col gap-y-2 px-6 py-8 dark:bg-[#321d4e] bg-[#c6abe9] rounded-xl h-full ${right ? "md:text-right md:items-end" : "text-left items-start"} justify-center`}>
         <h3 className={`tracking-widest text-4xl font-bold`}>{title}</h3>
         <div className="flex flex-col text-xl lg:max-w-[24vw]">
             {children}
