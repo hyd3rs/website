4b607d3027dc6867bbd592eabb1ed468b7a17a71
Dani <git@danny.works>
2024-05-06 11:39:28 -0700

    upgrade next.js, fix --turbo with mdx

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,18 +1,27 @@
-const withMDX = require('@next/mdx')();
+const withMDX = require("@next/mdx")({});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Configure `pageExtensions` to include MDX files
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    // Optionally, add any other Next.js config below
-    experimental: {
-        mdxRs: true,
-        turbo: {
-            rules: {
-                "*.mdx": ["@mdx-js/loader"]
-            }
-        }
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+  experimental: {
+    mdxRs: true,
+    turbo: {
+      /*rules: {
+        "*.mdx": ["@mdx-js/loader"],
+      },*/
+      resolveExtensions: [
+        ".mdx",
+        ".tsx",
+        ".ts",
+        ".jsx",
+        ".js",
+        ".mjs",
+        ".json",
+      ],
     },
+  },
 };
 
 module.exports = withMDX(nextConfig);
