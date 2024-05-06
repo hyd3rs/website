f517407cc2f107a6e3e03b739461650abd1cbc52
Dani <git@danny.works>
2024-05-06 11:39:43 -0700

    remove twitter link

--- apps/frontend/components/Layout.jsx
+++ apps/frontend/components/Layout.jsx
@@ -1,39 +1,61 @@
 import { MarkdownBar, MarkdownPath } from "@std/components/Base/Markdown";
 import { TabsDrawer } from "@std/components/Base/TabsDrawer";
 import { StandardButtonLink } from "@std/components/Interactables";
-import { BiEnvelope, BiLogoGithub, BiLogoTwitter, BiSolidPaperPlane, BiSolidUser } from "react-icons/bi";
+import {
+  BiEnvelope,
+  BiLogoGithub,
+  BiLogoTwitter,
+  BiSolidPaperPlane,
+  BiSolidUser,
+} from "react-icons/bi";
 import { FaLastfm } from "react-icons/fa";
 import { SiCodeberg } from "react-icons/si";
 
-
 export default async function BasePage({ children }) {
-    return (
-        <div className="flex flex-col gap-y-8 md:gap-y-0 h-full">
-            <main className="z-[0] md:px-6 flex flex-col items-start rounded-t-xl text-left w-full h-full pt-4 amd:py-4 transition-transform ease-in-out duration-250">
-                {children}
-            </main>
-        </div>
-    )
-};
+  return (
+    <div className="flex flex-col gap-y-8 md:gap-y-0 h-full">
+      <main className="z-[0] md:px-6 flex flex-col items-start rounded-t-xl text-left w-full h-full pt-4 amd:py-4 transition-transform ease-in-out duration-250">
+        {children}
+      </main>
+    </div>
+  );
+}
 
 export const Links = (props) => {
-    return (
-        <>
-            <StandardButtonLink icon href="mailto:hello@danny.works" external {...props}>
-                <BiEnvelope />
-            </StandardButtonLink>
-            <StandardButtonLink icon href="https://github.com/daniwasonline" external {...props}>
-                <BiLogoGithub />
-            </StandardButtonLink>
-            <StandardButtonLink icon href="https://codeberg.org/daniaintonline" external {...props}>
-                <SiCodeberg />
-            </StandardButtonLink>
-            <StandardButtonLink icon href="https://twitter.com/dani2off" external {...props}>
-                <BiLogoTwitter />
-            </StandardButtonLink>
-            <StandardButtonLink icon href="https://www.last.fm/user/daniaintonline" external {...props}>
-                <FaLastfm />
-            </StandardButtonLink>
-        </>
-    );
+  return (
+    <>
+      <StandardButtonLink
+        icon
+        href="mailto:hello@danny.works"
+        external
+        {...props}
+      >
+        <BiEnvelope />
+      </StandardButtonLink>
+      <StandardButtonLink
+        icon
+        href="https://github.com/daniwasonline"
+        external
+        {...props}
+      >
+        <BiLogoGithub />
+      </StandardButtonLink>
+      <StandardButtonLink
+        icon
+        href="https://codeberg.org/daniaintonline"
+        external
+        {...props}
+      >
+        <SiCodeberg />
+      </StandardButtonLink>
+      <StandardButtonLink
+        icon
+        href="https://www.last.fm/user/daniaintonline"
+        external
+        {...props}
+      >
+        <FaLastfm />
+      </StandardButtonLink>
+    </>
+  );
 };
